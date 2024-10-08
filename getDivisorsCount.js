ef4e348610531be77ec85482d819e95b8a53df82
Hien Nguyen <hientn179@gmail.com>
2024-10-07 22:18:33 -0700

    Work on splitStrings

--- getDivisorsCount.js
+++ getDivisorsCount.js
@@ -1,10 +1,11 @@
 function getDivisorsCount(n) {
   let count = 0;
+  //i * i <= n - iterate only up to the square root of n
   for (let i = 1; i * i <= n; i++) {
     if (n % i === 0) {
-      count++;
+      count++; // i is a divisor
       if (i !== n / i) {
-        count++;
+        count++; //n / i is also a divisor, but only if it's different from i
       }
     }
   }
---
+++ splitStrings.js
@@ -0,0 +1,12 @@
+//Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+//Examples:
+//'abc' =>  ['ab', 'c_']
+//'abcdef' => ['ab', 'cd', 'ef']
+function splitStrings(str) {
+  let arr = [];
+  for (i = 0; i < str.length; i += 2) {
+    let second = str[i + 1] || "_";
+    arr.push(str[i] + second);
+  }
+  return arr;
+}
