e012d3d529b3ef791643c2fd059f801f9ad00dbc
Hien Nguyen <hientn179@gmail.com>
2024-10-28 13:55:20 -0700

    Update tribonacci

--- fundamentals.tsx
+++ fundamentals.tsx
@@ -12,13 +12,8 @@ function countSheeps(sheep: []) {
 // You need to create a fibonacci function that given a signature array/list, returns the first n elements - signature included of the so seeded sequence.
 // Signature will always contain 3 numbers; n will always be a non-negative number; if n == 0, then return an empty array (except in C return NULL) and be ready for anything else which is not clearly specified.
 function tribonacci(signature: number[], n: number) {
-  let trib: number[] = signature;
-  if (n <= 1) {
-    return trib.slice(0, n);
-  }
   for (let i = 0; i < n - 3; i++) {
-    let sum = signature[i] + signature[i + 1] + signature[i + 2];
-    trib.push(sum);
+    signature.push(signature[i] + signature[i + 1] + signature[i + 2]);
   }
-  return trib;
+  return signature.slice(0, n);
 }
